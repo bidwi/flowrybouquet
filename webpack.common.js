a9f5cf806dd58d8eecbe6a2a6b08cfefe001b9b8
Billy Dwi Prakoso <billy2perakhoso@gmail.com>
2024-10-23 09:44:15 +0700

    changing docs to dist

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
@@ -35,7 +35,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'docs/'),
+          to: path.resolve(__dirname, 'dist/'),
         },
       ],
     }),
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, 'docs'),
+    static: path.resolve(__dirname, 'dist'),
     open: true,
     port: 9000,
     client: {
